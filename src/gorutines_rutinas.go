81cabc726b6c77210edc9e19098d544f8be2f263
Jesus David villalba <jesus.villalba@zalvadora.com>
2023-01-13 19:31:32 -0500

    gorutines o rutinas en go

---
+++ src/gorutines_rutinas.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func say(texto string, wg *sync.WaitGroup) {
+	defer wg.Done() // Esta linea se va a ejecutar hasta el final de la funcion, y de esta forma libera el gorutine del WaitGroup
+	fmt.Println(texto)
+}
+
+func main() {
+	var wg sync.WaitGroup // El paquete sync permite interacturar de forma primitiva con las gorutine. Variable que acomula un conjunto de gorutines y los va liberando poco a poco
+
+	fmt.Println("Hello")
+	wg.Add(1) // Indicamos que vamos a agregar 1 Gorutine al WaitGroup para que espere su ejecucion antes de que la gurutine base (main) muera, y así le de tiempo a la siguiente gorutine de ejecutarse
+	go say("world", &wg)
+
+	wg.Wait() // Funcion del WaitGroup que sirve para decirle al gorutine principal (main) que espere hasta que todas las gorutine del WaitGroup finalicen, es decir, hasta que se ejecute 'defer
+
+	go func(texto string) { // funcion anonima
+		fmt.Println(texto)
+	}("Adios")
+
+	time.Sleep(time.Second * 1)
+	//Funcion para que cuando llegue a esta linea espere el tiempo indicado (lo suficiente para que la Gorutine ejecute su funcion de forma concurrente)
+
+	// Nota: Para fines practicos se hace uso de la funcion Sleep(), pero en realidad NO es una buena practica, es mejor utilizar los WaitGroups
+}
